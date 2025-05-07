667ca8dfb511589463f18eac45d910bc4fa504b0
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-05-07 15:31:28 -0300

    FIFO and RR

--- src/app/components/process-spec/process-spec.component.ts
+++ src/app/components/process-spec/process-spec.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IProcess } from '../../interfaces/process.interface';
 import { ProcessType } from '../../enums/process-type.enum';
 import { ProcessStatus } from '../../enums/process-status.enum';
+import { Algorithm } from '../../enums/os.algorithm.enum';
 
 @Component({
   selector: 'app-process-spec',
@@ -24,16 +25,25 @@ export class ProcessSpecComponent implements OnInit {
   };
 
   paused: boolean = false;
-  interval: number = 1500;
+  cpuInterval: number = 1500;
+  ioInterval: number = 1500;
+  algorithm: Algorithm = Algorithm.ROUND_ROBIN;
 
   executing: IProcess | undefined;
 
   ngOnInit(): void {
+
+    //IO
+    setInterval(() => {
+      
+    }, this.ioInterval);
+
+    //CPU
     setInterval(() => {
       if (this.paused) return;
   
       if (this.executing) {
-        this.updateProcess();
+        this.updateProcess(this.algorithm);
         return;
       }
   
@@ -50,7 +60,7 @@ export class ProcessSpecComponent implements OnInit {
       }
   
       this.updateQueues();
-    }, this.interval);
+    }, this.cpuInterval);
   }
 
   private removeProcess(process: IProcess) {
@@ -59,16 +69,19 @@ export class ProcessSpecComponent implements OnInit {
     this.queues[2] = this.queues[2].filter(p => p.pid !== process.pid);
   }
 
-  private updateProcess() {
+  private updateProcess(algorithm: Algorithm): void {
     if (!this.executing) return;
   
     const process = this.executing;
   
     if (process.type == ProcessType.CPU) {
-      process.status = ProcessStatus.READY;
       process.instant = Date.now();
       process.remaining--;
   
+      if (algorithm === Algorithm.ROUND_ROBIN) {
+        process.status = ProcessStatus.READY;
+      }
+      
       if (process.remaining <= 0) {
         process.status = ProcessStatus.TERMINATED;
         this.removeProcess(process);
@@ -76,13 +89,16 @@ export class ProcessSpecComponent implements OnInit {
         this.executing = undefined;
         return;
       }
+  
       this.updateQueues();
+  
+      if (algorithm === Algorithm.ROUND_ROBIN) {
+        this.executing = undefined;
+      }
     } else {
       process.status = ProcessStatus.WAITING;
+      this.processUpdated.emit(process);
     }
-  
-    this.processUpdated.emit(process);
-    this.executing = undefined;
   }
 
   private getNext(): IProcess | undefined {
@@ -94,7 +110,7 @@ export class ProcessSpecComponent implements OnInit {
   private updateQueues() {
     let filteredList = this.processList.filter(p => p.status === ProcessStatus.READY);
 
-    this.queues[2] = filteredList.filter(p => p.priority == 2) .sort((a, b) => a.instant - b.instant);
+    this.queues[2] = filteredList.filter(p => p.priority == 2).sort((a, b) => a.instant - b.instant);
     this.queues[1] = filteredList.filter(p => p.priority == 1).sort((a, b) => a.instant - b.instant);
     this.queues[0] = filteredList.filter(p => p.priority == 0).sort((a, b) => a.instant - b.instant);
   }
---
+++ src/app/enums/os.algorithm.enum.ts
@@ -0,0 +1,4 @@
+export enum Algorithm {
+    ROUND_ROBIN,
+    FIFO
+}
